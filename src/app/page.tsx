56dd43ffa690e1dba44be45acbfd6ea557c07ad4
littlejohny2 <91983957+littlejohny2@users.noreply.github.com>
2023-12-07 21:24:03 -0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from 'next/image'
-import art from '../../public/battle_art.png'
 import googleearth from '../../public/corpspositionjune30.png'
 import Header from './components/header'
 
